4b613ffc3f4331214700c8c72d2524fd75bd35f5
konstantin zhilyaev <k926227@gmail.com>
2021-02-23 16:51:45 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+    let minValue = array[0];
+    array.forEach(item => minValue < item? minValue: minValue = item);
+    return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+    
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+    return array.reduce((sum, item) => sum + item, 0)/array.length;
+
 }
